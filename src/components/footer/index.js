b4b66ea4a104207a011d80d572c62020f3cdb9ae
Christian Engen <chris.a.engen@gmail.com>
2017-08-08 19:17:03 +0200

    Add some finishing glam to header & footer

---
+++ src/components/footer/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Constrainer from '../constrainer';
+import './footer.css';
+
+const Footer = () => (
+  <footer className="main-footer">
+    <Constrainer>
+      <ul>
+        <li>
+          © 2017 Christian Andreas Engen
+        </li>
+      </ul>
+    </Constrainer>
+  </footer>
+);
+
+export default Footer;
