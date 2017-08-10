d6c8438027589d989972e1d3670758fd9c65125b
Christian Engen <chris.a.engen@gmail.com>
2017-08-10 21:17:22 +0200

    Fix constrainer undefined

--- src/components/constrainer/index.js
+++ src/components/constrainer/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './constrainer.css';
 
-const Constrainer = (props) => (
-  <div className={`constrainer ${props.tighter && 'constrainer-tighter'}`}>
-    {props.children}
-  </div>
-)
+const Constrainer = (props) => {
+  let tight = props.tighter ? props.tighter : '';
+  return (
+    <div className={`constrainer${ tight}`}>
+      {props.children}
+    </div>
+  );
+}
 
 export default Constrainer;
