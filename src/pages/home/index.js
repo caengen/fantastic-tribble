083f6c5fab619a38c785f27a94b7e9f097213d08
Christian Engen <chris.a.engen@gmail.com>
2017-08-11 18:17:08 +0200

    Fix general space 🚀

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Page from '../Page';
 import Constrainer from '../../components/constrainer';
-import Editor from '../../components/editor';
+import CiaoEditor from '../../components/editor';
 import './home.css';
 
 const Home = () => (
     <Page className="home">
         <Constrainer>
             <h1>Choose your own adventure</h1>
-            <Editor />
+            <CiaoEditor />
         </Constrainer>
     </Page>
 );
